4f942bae773f7dde6158dbadc43203abcb130926
Damian Nowok <damian.nowok@gmail.com>
2013-11-10 16:26:35 +0100

    Pip
    Structure reorg to match pip requirements.

---
+++ prc/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='PRC',
+    version='0.9.0',
+    author='Damian Nowok',
+    author_email='damian.nowok@gmail.com',
+    packages=['prc', 'prc.test', 'prc.comm'],
+    scripts=['bin/example_prcclient.py','bin/example_prcserver.py'],
+    url='http://pypi.python.org/pypi/prc/',
+    license='LICENSE.txt',
+    description='Python Remote Console',
+    long_description=open('README.txt').read(),
+    install_requires=[],
+)
